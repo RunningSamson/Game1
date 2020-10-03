aece2c73861237ab4bfb9fb10dc7f6c909bc48bd
RunningSamson <bent.declerck@outlook.com>
2020-10-03 19:17:41 +0200

    Steam plugin toegevoegd
    Normaal werkt steam overlay enzo nu

--- Source/Game1.Target.cs
+++ Source/Game1.Target.cs
@@ -8,6 +8,7 @@ public class Game1Target : TargetRules
 	public Game1Target( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
+		bUsesSteam = true;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "Game1" } );
 	}
--- Source/Game1Editor.Target.cs
+++ Source/Game1Editor.Target.cs
@@ -8,6 +8,7 @@ public class Game1EditorTarget : TargetRules
 	public Game1EditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+		bUsesSteam = true;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.AddRange( new string[] { "Game1" } );
 	}
